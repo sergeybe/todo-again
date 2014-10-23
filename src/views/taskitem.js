554d37e9e2ab2670131c050fc707560eaa9e0367
Sergey N. Belinsky <sergeybe@gmail.com>
2014-10-23 22:13:25 +0600

    * Fixed bug this item view update.

--- src/views/taskitem.js
+++ src/views/taskitem.js
@@ -18,21 +18,10 @@ function(Marionette, moment, template1, template2) {
       'change @ui.checkbox': 'checkbox:changed'
     },
 
-    initialize: function(options) {
+    initialize: function() {
       this.today = moment(this.model.collection.today);
       this.id = 'task-' + this.model.cid;
       this.datetime = moment(this.model.get('datetime'));
-
-      if (this.model.get('complited')) {
-        this.datetimeClass = 'complited-task';
-      } else if (this.today.isBefore(this.datetime, 'day')) {
-        this.datetimeClass = 'future-task';
-      } else if (this.today.isAfter(this.datetime, 'day')) {
-        this.datetimeClass = 'overdue-task';
-      } else {
-        this.datetimeClass = 'today-task';
-      }
-
     },
 
     templateHelpers: function() {
@@ -52,10 +41,24 @@ function(Marionette, moment, template1, template2) {
       return _.template(template1);
     },
 
+    onBeforeRender: function() {
+      console.log("ItemView::onRender");
+      if (this.model.get('complited')) {
+        this.datetimeClass = 'complited-task';
+      } else if (this.today.isBefore(this.datetime, 'day')) {
+        this.datetimeClass = 'future-task';
+      } else if (this.today.isAfter(this.datetime, 'day')) {
+        this.datetimeClass = 'overdue-task';
+      } else {
+        this.datetimeClass = 'today-task';
+      }
+    },
+
     onCheckboxChanged: function() {
       console.log('checked');
       this.model.set('complited', !this.model.get('complited'));
       // this.model.save();
+      this.render();
     }
 
   });
